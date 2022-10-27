c6c82d769de6426677a32ab9a8bd069842a81989
zienabhussien <76661411+zienabhussien@users.noreply.github.com>
2022-10-27 15:44:49 +0200

    Or idk

--- app/src/main/java/com/lightfeather/friendskeep/presentation/AppModules.kt
+++ app/src/main/java/com/lightfeather/friendskeep/presentation/AppModules.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.lightfeather.friendskeep.domain.db.FriendDao
 import com.lightfeather.friendskeep.domain.db.FriendDatabase
 import com.lightfeather.friendskeep.ui.viewmodel.FriendViewModel
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -19,7 +20,7 @@ fun provideDao(friendDatabase: FriendDatabase): FriendDao =
     friendDatabase.getFriendDao()
 
 val friendDatabaseModule = module {
-    single { providesDatabase(get()) }
+    single { providesDatabase(application = androidApplication()) }
     single { provideDao(get()) }
 }
 
